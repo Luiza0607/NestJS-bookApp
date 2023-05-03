7a9c44629e683dbb2ebdd0e9ffd0a16a9fee9f78
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-05-03 21:28:12 +0200

    add model user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.userService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.userService.getById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+}
