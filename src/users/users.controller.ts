39b492247ec60bc496530eb098e47ffd7c641ac6
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-05-06 11:40:47 +0200

    add book like endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,8 +4,12 @@ import {
   Param,
   ParseUUIDPipe,
   NotFoundException,
+  Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -21,5 +25,15 @@ export class UsersController {
     const user = await this.userService.getById(id);
     if (!user) throw new NotFoundException('User not found');
     return user;
+  }
+
+  @Delete('/:id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.userService.getById(id)))
+      throw new NotFoundException('User not found');
+    await this.userService.deleteById(id);
+    return { success: true };
   }
 }
